839b5acc251c57d39f0f94c09e23cae879871810
Scutlhj <124867310+Scutlhj@users.noreply.github.com>
2023-05-03 20:39:33 +0800

    Update AdminServlet.java

--- src/pers/sakurasora/onlinestore/web/servlet/AdminServlet.java
+++ src/pers/sakurasora/onlinestore/web/servlet/AdminServlet.java
@@ -1,15 +1,18 @@
 package pers.sakurasora.onlinestore.web.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSONObject;
 import pers.sakurasora.onlinestore.constant.Constant;
 import pers.sakurasora.onlinestore.entity.Administrator;
+import pers.sakurasora.onlinestore.entity.User;
 import pers.sakurasora.onlinestore.service.AdminService;
 import pers.sakurasora.onlinestore.service.impl.AdminServiceImpl;
 import pers.sakurasora.onlinestore.web.servlet.base.BaseServlet;
@@ -52,7 +55,16 @@ public class AdminServlet extends BaseServlet {
 				request.setAttribute("error", "用户名或密码不正确");;
 				return "/jsp/admin/index.jsp";
 			}
-			
+
+			JSONObject jsonObj = new JSONObject();
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+			jsonObj.put("login_time", dtf.format(LocalDateTime.now()));
+			jsonObj.put("login_ip", request.getRemoteAddr());
+			jsonObj.put("account", admin.getAccount());
+			String json_to_string = JSONObject.toJSONString(jsonObj);
+			System.out.println(json_to_string);
+			writeFile(json_to_string,"C:\\Users\\Administrator\\Desktop\\Adminlogin.json");
+
 			request.getSession().setAttribute("admin", admin); // 保存管理员登录状态
 			
 			response.sendRedirect(request.getContextPath() + "/jsp/admin/home.jsp"); //重定向到 管理中心/jsp/admin/home.jsp
@@ -70,7 +82,16 @@ public class AdminServlet extends BaseServlet {
 	public String logout(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 //		request.getSession().invalidate(); // 清除session中的所有信息
-		
+		Administrator admin = (Administrator)request.getSession().getAttribute("admin");
+
+		JSONObject jsonObj = new JSONObject();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		jsonObj.put("logout_time", dtf.format(LocalDateTime.now()));
+		jsonObj.put("logout_ip", request.getRemoteAddr());
+		jsonObj.put("account", admin.getAccount());
+		String json_to_string = JSONObject.toJSONString(jsonObj);
+		System.out.println(json_to_string);
+		writeFile(json_to_string,"C:\\Users\\Administrator\\Desktop\\Adminlogout.json");
 		request.getSession().removeAttribute("admin"); // 将admin从session中移除
 		
 		PrintWriter pw = response.getWriter();
@@ -80,4 +101,25 @@ public class AdminServlet extends BaseServlet {
 		return null;
 	}
 
+	public void writeFile(String json, String FilePath) {
+
+		try {
+			File file = new File(FilePath);
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			// true = append file
+			FileWriter fileWritter = new FileWriter(file,true);
+			BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+			bufferWritter.write(json);
+			bufferWritter.newLine();
+			bufferWritter.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
